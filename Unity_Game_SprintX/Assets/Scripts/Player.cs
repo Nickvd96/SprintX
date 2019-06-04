1bd87ea89cb97e920f2fd2afedab539aeee604db
Driel,Nick N. van <n.vandriel@student.fontys.nl>
2019-06-04 12:23:15 +0200

    insert  sprites and player movement
    nieuwe sprites toegevoegd en begin gemaakt aan de movement van de player

---
+++ Unity_Game_SprintX/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private Rigidbody2D myRigidbody;
+
+	[SerializeField]
+	private float movementSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	float horizontal = Input.GetAxis("Horizontal");
+    	//Debug.log(horizontal);
+        HandleMovement(horizontal);
+    }
+
+    private void HandleMovement(float Horizontal)
+    {
+
+    	myRigidbody.velocity = new Vector2(Horizontal * movementSpeed,myRigidbody.velocity.y);
+    }
+}
